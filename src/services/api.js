b0ae2f1ce0c58d838058619cb338c31e00472171
Nafish Imtiaz <151770553+Nafish32@users.noreply.github.com>
2026-06-14 20:44:15 +0600

    changed the api

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 
-//const BASE_URL = 'https://api.tatomal.me';
-const BASE_URL = 'http://localhost:8000';
+const BASE_URL = 'https://api.tatomal.me';
+//const BASE_URL = 'http://localhost:8000';
 
 
 // Create axios instance with default config
